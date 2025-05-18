7645b7d17542f75cac0bdc028e8e13a7e96206e8
hm <squirrel309@naver.com>
2025-05-18 19:20:48 +0900

    DOM Test Handler - userEvent 사용

--- sundae-starter/src/page/summary/summry-form.jsx
+++ sundae-starter/src/page/summary/summry-form.jsx
@@ -2,26 +2,36 @@ import { useState } from "react";
 
 export default function SummryForm() {
   const [agree, setAgree] = useState(false);
+  const [over, setOver] = useState(false);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <label>
-        <input
-          type="checkbox"
-          name="agree"
-          onChange={(e) => {
-            setAgree(e.target.checked ? true : false);
-          }}
-        />
-        I agree to
-      </label>
-      <button name="formSubmit" disabled={!agree}>
-        confirm
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <label>
+          <input
+            type="checkbox"
+            name="agree"
+            onChange={(e) => {
+              setAgree(e.target.checked ? true : false);
+            }}
+          />
+          I agree to
+        </label>
+        <button name="formSubmit" disabled={!agree}>
+          confirm
+        </button>
+      </div>
+      <button
+        onMouseOver={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}
+      >
+        overTarget
       </button>
-    </div>
+      {over && <div>test</div>}
+    </>
   );
 }
--- sundae-starter/src/page/summary/summry-form.test.jsx
+++ sundae-starter/src/page/summary/summry-form.test.jsx
@@ -1,16 +1,35 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import SummryForm from "./summry-form";
+import userEvent from "@testing-library/user-event";
 
-test("체크 박스 플로우", () => {
+test("체크 박스 플로우", async () => {
+  const user = await userEvent.setup();
   render(<SummryForm />);
   //   btn
-  const btn = screen.getByRole("button");
+  const btn = screen.getByText("confirm");
   expect(btn).toBeDisabled();
 
   const checkboxElement = screen.getByRole("checkbox");
   expect(checkboxElement).not.toBeChecked();
 
   //   chkbox
-  fireEvent.click(checkboxElement);
+  await user.click(checkboxElement);
   expect(btn).toBeEnabled();
+
+  await user.click(checkboxElement);
+  expect(btn).toBeDisabled();
+});
+
+// 사용자 기준으로 함
+test("팝업 핸들링", async () => {
+  const user = await userEvent.setup();
+  render(<SummryForm />);
+
+  const target = screen.getByText(/overTarget/i);
+  await user.hover(target);
+  const popover = screen.queryByText("test");
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(target);
+  expect(popover).not.toBeInTheDocument();
 });
